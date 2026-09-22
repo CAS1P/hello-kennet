d03f0f26a4ea932ac0a79c7a597e0667572d02aa
CAS1P <179693332+CAS1P@users.noreply.github.com>
2026-09-22 12:43:12 +0200

    Implement service worker install and activate listeners
    Added event listeners for service worker installation and activation.

--- js.js
+++ js.js
@@ -1,7 +1,12 @@
 if ('serviceWorker' in navigator) {
    navigator.serviceWorker.register("/serviceworker.js");
 }
-
+self.addEventListener("install", event => {
+   console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+   console.log("Service worker activated");
+});
 const CACHE_NAME = `temperature-converter-v1`;
    
 // Use the install event to pre-cache all initial resources.
