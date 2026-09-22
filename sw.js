8e21e59615d0b1a846bc2354b14bb8ec972b3639
CAS1P <179693332+CAS1P@users.noreply.github.com>
2026-09-22 13:57:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'hello-kennet-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([
+      './',
+      './hello-kennet.html',
+      './style.css'
+    ]);
+  })());
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+    try {
+      const fetchResponse = await fetch(event.request);
+      cache.put(event.request, fetchResponse.clone());
+      return fetchResponse;
+    } catch (e) {
+      // Network failed and nothing cached for this request.
+    }
+  })());
+});
